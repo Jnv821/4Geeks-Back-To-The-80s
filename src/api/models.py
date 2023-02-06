266b868b75a75966e152272701e907661040953d
Jnv821 <jnv821@gmail.com>
2023-02-06 22:32:56 +0000

    Added Many-to-Many relationship table and modified user model to respresent these changes.

--- src/api/models.py
+++ src/api/models.py
@@ -2,12 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+favorites = db.Table('favorites',
+    db.Column('favorite_id', db.Integer, primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('song_id', db.Integer, db.ForeignKey('song.id')),
+    db.Column('album_id', db.Integer, db.ForeignKey('album.id')),
+    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorited_songs = db.relationship('Song',secondary=favorites, backref='favorited_by')
+    favorited_albums = db.relationship('Album', secondary=favorites, backref='favorited_by')
+    favorited_artists = db.relationship('Artist', secondary=favorites, backref='favorited_by')
 
     def __repr__(self):
         return f'<User {self.email}>'
