a178637b434d208d7be8c1ac2d7667ddc05e58d5
sandrafepu <sandrafepu@gmail.com>
2023-02-07 20:14:23 +0000

    Segunda prueba errores

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,17 +4,12 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
+  return (
+	<div className="text-center mt-5">
+			<h1>BACK TO THE 80s</h1>
+		
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
 				<a href="https://start.4geeksacademy.com/starters/react-flask">
