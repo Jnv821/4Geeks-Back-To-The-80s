19ac26ed270d28ebdfd315554db16d4d55892ab6
Jnv821 <jnv821@gmail.com>
2023-02-14 20:56:27 +0000

    Fixed some underliying issues with the DB

--- src/api/models.py
+++ src/api/models.py
@@ -5,9 +5,7 @@ db = SQLAlchemy()
 favorites = db.Table('favorites',
     db.Column('favorite_id', db.Integer, primary_key=True),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('song_id', db.Integer, db.ForeignKey('song.id')),
     db.Column('album_id', db.Integer, db.ForeignKey('album.id')),
-    db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'))
 )
 
 class User(db.Model):
@@ -17,10 +15,8 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     description = db.Column(db.String(255), unique=False, nullable=True)
-    favorited_songs = db.relationship('Song',secondary=favorites, backref='favorited_by')
     favorited_albums = db.relationship('Album', secondary=favorites, backref='favorited_by')
-    favorited_artists = db.relationship('Artist', secondary=favorites, backref='favorited_by')
-
+    
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -31,30 +27,6 @@ class User(db.Model):
             "username": self.username,
             "is_active": self.is_active
             # do not serialize the password, its a security breach
-        }
-
-class Song(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    spotify_url = db.Column(db.String(2048), unique=True, nullable=False)
-    def __repr__(self):
-        return  f'<Song {self.id} - {self.spotify_url}>'
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "spotify_url" : self.spotify_url
-        }
-
-class Artist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    spotify_url = db.Column(db.String(2048), unique=True, nullable=False)
-    def __repr__(self):
-        return  f'<Artist {self.id} - {self.spotify_url}>'
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "spotify_url" : self.spotify_url
         }
 
 class Album(db.Model):
