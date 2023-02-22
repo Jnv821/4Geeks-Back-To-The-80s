02a22bf4770c73fb1b8b6accb9b6bb36bdea90f4
Jose Raimundo Del Rio <jrdelriodom@gmail.com>
2023-02-22 22:51:38 +0000

    Avance en el sistema de autenticación

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,16 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, Users
 from api.utils import generate_sitemap, APIException
-import app
-from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
 
-app = Flask(__name__)
-
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = "super-secret"
-jwt = JWTManager(app)
-
+# Create flask api
 api = Blueprint('api', __name__)
-
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -27,9 +21,14 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@api.route('/token', methods=['POST'])
-def create_token():
-    
-    response_body = app.spotify_token
 
-    return jsonify(response_body), 200
+@api.route("/token", methods=["POST"])
+def create_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
