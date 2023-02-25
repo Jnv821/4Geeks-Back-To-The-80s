19b3d6bb34d2853fadbf3b9ccc0fd324783f8030
sandrafepu <sandrafepu@gmail.com>
2023-02-25 15:15:06 +0000

    Primera subida de vista

--- src/front/js/pages/single.jsx
+++ src/front/js/pages/single.jsx
@@ -3,38 +3,40 @@ import "../../styles/single.css";
 import Navbar from "../component/navbar.js";
 
 export const Single = (props) => {
-
   let [album, setAlbum] = useState([]);
-
+console.log(props)
   useEffect(() => {
-    fetch("https://api.spotify.com/v1/artists/3fMbdgg4jU18AjLCKBhRSm")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      setAlbum(data);
+    fetch(
+      "https://3001-jnv821-4geeksbacktothe8-bunat2pqo45.ws-eu88.gitpod.io/api/albums/1"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data.album);
+        setAlbum(album=data.album);
+        console.log(album);
       });
   }, []);
-  
-    return (
-      <>
+
+  return (
+    <>
       <Navbar />
       <div className="container-single">
         <div className="row">
           <div className="single-photo col-sm-4">
-              <img src="https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png" width="500" height="500" />
+              <img src={album.album_cover} width="500" height="500" />
           </div>
               <div className="information-single col-sm-8">
-                <p className="information-album">- Name :</p>
-                 <br/>
-                <p className="information-album">- Release date :</p>
-                 <br/>
-                <p className="information-album">- Copyrights :</p>
-                 <br/>
-                <p className="information-album">- Total tracks :</p>
-                 <br/>
-              </div>
+            <p className="information-album">- Name : {album.title}</p>
+            <br />
+            <p className="information-album">- Release date : {album.release_year}</p>
+            <br />
+            <p className="information-album">- Copyrights :</p>
+            <br />
+            <p className="information-album">- Total tracks :</p>
+            <br />
+          </div>
         </div>
       </div>
-      </>
-    );
-    };
+    </>
+  );
+};
