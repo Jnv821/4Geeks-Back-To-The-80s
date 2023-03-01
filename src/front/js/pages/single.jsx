d9a3334d09c3233b14895464b8b7f4961dcd0c07
sandrafepu <sandrafepu@gmail.com>
2023-03-01 12:57:06 +0000

    Vista single con problemas token/spotify

--- src/front/js/pages/single.jsx
+++ src/front/js/pages/single.jsx
@@ -1,40 +1,50 @@
+
 import React, { useState, useEffect, useContext } from "react";
 import "../../styles/single.css";
 import Navbar from "../component/navbar.js";
+import {useLocation} from 'react-router-dom'
+
 
 export const Single = (props) => {
-
   let [album, setAlbum] = useState([]);
 
+const url = useLocation().pathname
   useEffect(() => {
-    fetch("https://api.spotify.com/v1/artists/3fMbdgg4jU18AjLCKBhRSm")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      setAlbum(data);
+    fetch(process.env.BACKEND_URL + "/api" + url)
+      .then((response) => response.json())
+      .then((data) => {
+        setUrl(data);
+        console.log(data);
       });
   }, []);
-  
-    return (
-      <>
+
+  /* fetch(album.album.spotify_url, {
+     method: 'GET',
+     headers: {
+     'Authorization': "Bearer " + token
+     }
+      })*/
+
+  return (
+    <>
       <Navbar />
       <div className="container-single">
-        <div className="row">
-          <div className="single-photo col-sm-4">
-              <img src="https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png" width="500" height="500" />
-          </div>
-              <div className="information-single col-sm-8">
-                <p className="information-album">- Name :</p>
-                 <br/>
-                <p className="information-album">- Release date :</p>
-                 <br/>
-                <p className="information-album">- Copyrights :</p>
-                 <br/>
-                <p className="information-album">- Total tracks :</p>
-                 <br/>
-              </div>
+        <div className="single-photo">
+          <img src={album.album_cover} width="500" height="500" />
         </div>
+          <div className="container-information">
+              <div className="information-album"><h3 >- ARTISTA : <br/>{album.album ? album.album.artist.name : <p>Loading ...</p>}</h3></div>
+                <br />
+              <div className="information-author"><h3 >- ALBUM : <br/>{album.album ? album.album.name : <p>Loading ...</p>}</h3></div>
+                <br />
+              <div className="information-year"><h3 >- POPULARIDAD : <br/>{album.album ? album.album.popularity : <p>Loading ...</p>}</h3></div>
+                 <br />
+              <div className="information-year"><h3 >- FECHA DE LANZAMIENTO : <br/>{album.album ? album.album.release_date : <p>Loading ...</p>}</h3></div>
+                 <br />
+              <div className="information-year"><h3 >- NÚMERO DE CANCIONES: <br/>{album.album ? album.album.total_tracks : <p>Loading ...</p>}</h3></div>
+          </div>
       </div>
-      </>
-    );
-    };
+        
+    </>
+  );
+};
