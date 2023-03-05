148968925e01be5e3f8876507539547db375776a
sandrafepu <sandrafepu@gmail.com>
2023-03-05 18:09:44 +0000

    Errro de sintaxis en app.py línea 85.

--- migrations/versions/76ee8db2e2f7_.py
+++ migrations/versions/76ee8db2e2f7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3b1a04fe0a17
+Revision ID: 76ee8db2e2f7
 Revises: 
-Create Date: 2023-03-02 19:18:15.488953
+Create Date: 2023-03-05 12:02:13.035732
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3b1a04fe0a17'
+revision = '76ee8db2e2f7'
 down_revision = None
 branch_labels = None
 depends_on = None
